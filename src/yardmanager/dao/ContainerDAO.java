ba3c6a7102f9c15b5fcc6e2224f2832b79be9822
maxetron <emaxedon@gmail.com>
2015-08-31 00:08:59 -0400

    refactored company class dao

--- src/yardmanager/dao/ContainerDAO.java
+++ src/yardmanager/dao/ContainerDAO.java
@@ -16,58 +16,49 @@ import java.util.*;
  */
 public class ContainerDAO {
 	private Connection conn;
+	private CompanyDAO companyDAO;
 	
-	public ContainerDAO(Connection conn) {
+	public ContainerDAO(Connection conn, CompanyDAO companyDAO) {
 		this.conn = conn;
+		this.companyDAO = companyDAO;
 	}
 	
 	public void create(Container container) {
 		try {
-			conn.createStatement().executeUpdate("INSERT INTO Containers (ContainerID, CompanyID, Size, Mass, xPos, yPos, Level, Coverage, Type, ISOCode, Acceptance, Seal, TruckCode, TruckName, DateOfManufacture, TruckLicense, InspectorName, DateIn, DateOut, Comments, Colour, Full) VALUES ('" + 
+			conn.createStatement().executeUpdate("INSERT INTO Containers (Id, CustomerId, Type, ISOCode, SealNumber, Colour, Size, Mass, xPos, yPos, Level, Coverage, DateManufactored, Full) VALUES ('" + 
 				container.getId() + "', '" + 
-				container.getCustomerId() + "', '" + 
+				container.getCustomer().getId() + "', '" + 
+				container.getType() + "', '" + 
+				container.getISOCode() + "', '" + 
+				container.getSealNumber() + "', '" + 
+				container.getColour() + "', '" + 
 				container.getSize() + "', '" + 
 				container.getMass() + "', '" + 
 				container.getxPos() + "', '" + 
 				container.getyPos() + "', '" + 
 				container.getLevel() + "', '" + 
 				container.getCoverage() + "', '" + 
-				container.getType() + "', '" + 
-				container.getISOCode() + "', '" + 
-				container.getAcceptance() + "', '" + 
-				container.getSeal() + "', '" + 
-				container.getTruckCode() + "', '" + 
-				container.getTruckName() + "', '" + 
-				container.getInspectorName() + "', '" + 
-				container.getDateIn() + "', '" + 
-				container.getDateOut() + "', '" + 
-				container.getComments() + "', '" + 
-				container.getColour() + "', '" + 
+				container.getDateManufactured() + "', '" + 
 				container.isFull() + "')");
 		} catch (SQLException e) { System.out.println("Failed to create container: " + e); }
 	}
 	
 	public void update(Container container) {
 		try {
-			conn.createStatement().executeUpdate("UPDATE Containers SET CompanyID='" + container.getCustomerId() + 
+			conn.createStatement().executeUpdate("UPDATE Containers SET CustomerId='" + container.getCustomer().getId() + 
+					"', Type='" + container.getType() + 
+					"', ISOCode='" + container.getISOCode() + 
+					"', SealNumber='" + container.getSealNumber() + 
+					"', Colour='" + container.getColour() + 
 					"', Size='" + container.getSize() + 
 					"', Mass='" + container.getMass() + 
 					"', xPos='" + container.getxPos() + 
 					"', yPos='" + container.getyPos() + 
 					"', Level='" + container.getLevel() + 
 					"', Coverage='" + container.getCoverage() + 
-					"', Type='" + container.getType() + 
-					"', ISOCode='" + container.getISOCode() + 
-					"', Acceptance='" + container.getAcceptance() + 
-					"', Seal='" + container.getSeal() + 
-					"', TruckCode='" + container.getTruckCode() + 
-					"', InspectorName='" + container.getInspectorName() + 
-					"', DateIn='" + container.getDateIn() + 
-					"', DateOut='" + container.getDateOut() + 
-					"', Comments='" + container.getComments() + 
-					"', Colour='" + container.getColour() + 
+					"', DateManufactored='" + container.getDateManufactured() + 
 					"', Full='" + container.isFull() + 
-					"' WHERE ContainerID='" + container.getId() + "'");
+					"' WHERE Id='" + container.getId() + "'");
 		} catch (SQLException e) { System.out.println("Failed to update container: " + e); }
 	}
 	
@@ -76,30 +67,22 @@ public class ContainerDAO {
 		
 		try{
 			ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Containers");
-			
+
 			while(rs.next()) {
 				Container container = new Container(
-						rs.getString("ContainerID"),
-						rs.getString("CompanyID"),
+						rs.getString("Id"),
+						companyDAO.find(rs.getString("CustomerId")),
+						rs.getString("Type"),
+						rs.getString("ISOCode"),
+						rs.getString("SealNumber"),
+						rs.getString("Colour"),
 						rs.getInt("Size"),
 						rs.getInt("Mass"),
 						rs.getInt("xPos"),
 						rs.getInt("yPos"),
 						rs.getInt("Level"),
 						rs.getInt("Coverage"),
-						rs.getString("Type"),
-						rs.getString("ISOCode"),
-						rs.getString("Acceptance"),
-						rs.getString("Seal"),
-						rs.getString("TruckCode"),
-						rs.getString("TruckName"),
-						rs.getString("DateOfManufacture"),
-						rs.getString("TruckLicense"),
-						rs.getString("InspectorName"),
-						rs.getString("DateIn"),
-						rs.getString("DateOut"),
-						rs.getString("Comments"),
-						rs.getString("Colour"),
+						rs.getDate("DateManufactured"),
 						rs.getBoolean("Full"));
 				
 				containers.add(container);
@@ -111,34 +94,26 @@ public class ContainerDAO {
 		return containers;
 	}
 	
-	public Container find(int containerID) {
+	public Container find(int id) {
 		try {
-			ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Containers WHERE ContainerID='" + containerID + "' AND Status='still in'");
+			ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Containers WHERE Id='" + id + "' AND Status='still in'");
 			
 			if(rs.next()) {
 				Container container = new Container(
-					rs.getString("ContainerID"),
-					rs.getString("CompanyID"),
-					rs.getInt("Size"),
-					rs.getInt("Mass"),
-					rs.getInt("xPos"),
-					rs.getInt("yPos"),
-					rs.getInt("Level"),
-					rs.getInt("Coverage"),
-					rs.getString("Type"),
-					rs.getString("ISOCode"),
-					rs.getString("Acceptance"),
-					rs.getString("Seal"),
-					rs.getString("TruckCode"),
-					rs.getString("TruckName"),
-					rs.getString("DateOfManufacture"),
-					rs.getString("TruckLicense"),
-					rs.getString("InspectorName"),
-					rs.getString("DateIn"),
-					rs.getString("DateOut"),
-					rs.getString("Comments"),
-					rs.getString("Colour"),
-					rs.getBoolean("Full"));
+						rs.getString("Id"),
+						companyDAO.find(rs.getString("CustomerId")),
+						rs.getString("Type"),
+						rs.getString("ISOCode"),
+						rs.getString("SealNumber"),
+						rs.getString("Colour"),
+						rs.getInt("Size"),
+						rs.getInt("Mass"),
+						rs.getInt("xPos"),
+						rs.getInt("yPos"),
+						rs.getInt("Level"),
+						rs.getInt("Coverage"),
+						rs.getDate("DateManufactured"),
+						rs.getBoolean("Full"));
 				
 				rs.close();
 				
