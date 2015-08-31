ee629c89c0b7462ba0dbebca510131f7705dcbe3
maxetron <emaxedon@gmail.com>
2015-08-31 00:09:51 -0400

    stable

--- src/yardmanager/dao/Database.java
+++ src/yardmanager/dao/Database.java
@@ -14,8 +14,6 @@ public class Database {
 	Connection conn;
 		
 	public Database(String source) {
-		//String dburl = "jdbc:ucanaccess://C:/Users/Home/My Documents/java programs/containers.mdb";  
-		
 		try { 
 			this.conn = DriverManager.getConnection(source);	
 		} catch (Exception e) { 
